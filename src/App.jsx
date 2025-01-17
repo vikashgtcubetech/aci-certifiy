b10a6e49fea5b7d55f806152b3a271b01f6f02d3
vikashgtcubetech <vikashsaini6640@gmail.com>
2025-01-17 09:55:04 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+// import './App.css'
+// import Certificate from './Certificate'
+import PasswordProtectedPage from './component/PasswordProtectedPage'
+
+function App() {
+
+  return (
+    <>
+     <PasswordProtectedPage>
+     </PasswordProtectedPage>
+     
+    </>
+  )
+}
+
+export default App
